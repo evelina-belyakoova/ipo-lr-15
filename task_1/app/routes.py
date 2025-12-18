1cf9af3527d0ff2f4fa49a532e7383e671498a92
Evelina <belakovaevelina84@gmail.com>
2025-12-18 10:55:28 +0300

    Добавлены изменения

--- task_1/app/routes.py
+++ task_1/app/routes.py
@@ -15,3 +15,18 @@ def hello(name):
 @main.route('/square/<int:number>')
 def square(number):
     return f'Квадрат числа {number} равен {number ** 2}'
+
+@main.route('/json', methods=['POST'])
+def json():
+    data = request.get_json()
+
+    if not data or 'name' not in data or 'age' not in data:
+        print(f"Некорректные данные")
+
+    name = data['name']
+    age = data['age']
+    return jsonify({"message": f"Привет, {name}! Тебе {age} лет."})
+
+@main.route('/multiply/<int:a>/<int:b>')
+def multiply(a, b):
+    return f"Произведение чисел {a} и {b} равно {a * b}"
